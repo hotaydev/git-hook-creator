ca981991a22d912fb93be501bfc3ea7f9c4c29dc
TaylorHo <taylor@hotay.dev>
2024-04-18 15:13:58 -0300

    feat: creating hooks

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,63 +1,63 @@
 import * as p from '@clack/prompts';
 import path from 'node:path';
 import { readFile, writeFile, access, mkdir } from 'node:fs/promises';
-import { readFileSync } from 'node:fs';
+import { GitHooks } from '../models/git_hooks';
 
 export default class MainCli {
+  private gitHooksScript = 'git config --local core.hooksPath .hooks/';
   private packageJsonPath = path.join(path.dirname(''), 'package.json');
   private hooksFolderPath = path.join(path.dirname(''), '.hooks');
-  private preCommitPath = path.join(path.dirname(''), '.hooks', 'pre-commit');
 
   public async prompt(): Promise<void> {
     p.intro('Welcome to the git hook creator!');
 
-    p.log.info('We will create a .hooks folder and edit your prepare script.');
-    // seria legal perguntar isso só a primeira vez que rodasse
-    const response = await p.confirm({
-      message: 'Are you okay with this? Want to continue?',
+    const hook: string = (await p.select({
+      message: 'What hook do you want to create?',
+      options: GitHooks.map((hook) => {
+        return {
+          value: hook.hook,
+          label: hook.hook,
+          hint: hook.link,
+        };
+      }),
+      maxItems: 8,
+    })) as string;
+
+    const { scripts } = await this.getPackageJsonContent();
+    let scriptsChoosed: string[] = [];
+
+    if (!scripts) {
+      p.log.info('We couldn\'t find any script in your package.json.\nEnsure you have a script there so we cna include it into your git hook.');
+    } else {
+      scriptsChoosed = (await p.multiselect({
+        message: `Which scripts you want to execute on the ${hook} hook?`,
+        options: Object.keys(scripts).map((key) => ({
+          label: key,
+          value: key,
+        })),
+      })) as string[];
+    }
+
+    // TODO: In the future we can choose which of these points need to be shown, based on the current user configuration.
+    p.log.info(`For it to be done we will:\n- Create a .hooks folder if it doesn't already exist\n- Edit your prepare script in package.json\n- Create the .hooks/${hook} file`);
+
+    const okayToContinue = await p.confirm({
+      message: 'Do you want to apply changes now?'
     });
 
-    if (!response) {
+    if (!okayToContinue) {
       p.outro('Okay, we will not touch your file system ;)');
       process.exit(0);
     }
 
     this.createDotHooksFolder();
     this.createPrepareScript();
-
-    const { scripts }: Record<string, string> =
-      await this.getPackageJsonContent();
-
-    if (!scripts) {
-      p.outro(
-        'Could not find a declared scripts section in your package.json. :('
-      );
-      process.exit(0);
-    }
-
-    const selectedScript = await p.select({
-      message: 'Select the script you want to add as a hook...',
-      options: Object.entries(scripts).map(([key, value]) => ({
-        label: key,
-        value,
-      })),
-    });
-
-    const selectedScriptKey = Object.entries(scripts).find(
-      ([, value]) => value === selectedScript
-    )?.[0];
-
-    if (!selectedScriptKey) {
-      p.outro('Could not find execute the selected script!');
-      process.exit(0);
-    }
-
-    await this.addScriptToPreCommit(selectedScriptKey);
+    this.createHookFile(hook, scriptsChoosed);
 
     p.outro('Your hooks were added in .hooks folder.');
   }
 
-  private async getPackageJsonContent() {
+  private async getPackageJsonContent(): Promise<Record<string, any>> {
     const file = await readFile(this.packageJsonPath, {
       encoding: 'utf-8',
     });
@@ -91,16 +91,16 @@ export default class MainCli {
 
     if (!packageJson.scripts.prepare) {
       needToWriteFile = true;
-      packageJson.scripts.prepare = 'git config --local core.hooksPath .hooks/';
+      packageJson.scripts.prepare = this.gitHooksScript;
     } else if (
       !packageJson.scripts.prepare.includes(
-        'git config --local core.hooksPath .hooks/'
+        this.gitHooksScript
       )
     ) {
       needToWriteFile = true;
       packageJson.scripts.prepare =
         packageJson.scripts.prepare +
-        ' && git config --local core.hooksPath .hooks/';
+        ' && ' + this.gitHooksScript;
     }
 
     if (needToWriteFile)
@@ -111,41 +111,47 @@ export default class MainCli {
     // TODO: adjust this writeFile to match the user's identation instead of assuming it's 2
   }
 
-  private async addScriptToPreCommit(selectedScriptKey: string) {
-    const file = await readFile(this.preCommitPath, {
-      encoding: 'utf-8',
+  private async createHookFile(hook: string, scripts: string[]) {
+    const firstline = '#!/usr/bin/env sh';
+    const lastline = 'git add .';
+
+    const hookPath = path.join(path.dirname(''), '.hooks', hook);
+    let file: string;
+
+    try {
+      file = await readFile(hookPath, {
+        encoding: 'utf-8',
+      });
+
+      // TODO: add options to choose what to add/remove from the original hook file
+      const willContinue = await p.confirm({
+        message: `The file .hooks/${hook} already exists. Do you want to overwrite it?`
+      });
+
+      if (!willContinue) {
+        p.outro('Okay, we will not touch your file system ;)');
+        process.exit(0);
+      }
+
+    } catch (error) {
+      file = '' ;
+    }
+
+    const lines: string[] = [];
+    if (file) lines.push(...(file.split('\n').filter((line) => (line !== firstline) && (line !== lastline) && line !== '')));
+
+    scripts.forEach((script) => {
+      lines.push(`npm run ${script}`);
     });
 
-    if (!file) {
-      p.log.error(
-        'Could not read pre-commit file.\nCheck if .hooks/pre-commit file was created.'
-      );
-      process.exit(1);
-    }
+    const finalFile = [
+      firstline,
+      '', // space
+      ...lines,
+      '', // space
+      lastline,
+    ];
 
-    // Split the file content by lines
-    const lines = file.split('\n');
-
-    // Check if git add . already exists in the script
-    const gitAddIndex = lines.findIndex(line =>
-      line.trim().startsWith('git add .')
-    );
-
-    // If git add . exists, remove it from the script
-    if (gitAddIndex !== -1) {
-      lines.splice(gitAddIndex, 1);
-    }
-
-    // Append the new script command
-    lines.push(`npm run ${selectedScriptKey}`);
-
-    // Add git add . at the end of the script
-    lines.push('\ngit add .');
-
-    // Join the lines back into a single string
-    const newFile = lines.join('\n');
-
-    // Write the updated script content back to the file
-    await writeFile(this.preCommitPath, newFile);
+    await writeFile(hookPath, finalFile.join('\n'));
   }
 }
