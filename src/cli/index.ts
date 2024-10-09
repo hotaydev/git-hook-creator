d0c38064df1400a957bc257e1793c333d6f65720
TaylorHo <taylor@hotay.dev>
2024-10-09 10:37:43 -0300

    chore: upgraded dependencies and added new linter

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,201 +1,208 @@
-import * as p from '@clack/prompts';
-import path from 'node:path';
-import { readFile, writeFile, access, mkdir, chmod } from 'node:fs/promises';
-import { GitHooks } from '../models/git_hooks';
-import handleCancel from '../utils/handle_cancel';
+import { access, chmod, mkdir, readFile, writeFile } from "node:fs/promises";
+import path from "node:path";
+import * as p from "@clack/prompts";
+import { GitHooks } from "../models/git_hooks";
+import handleCancel from "../utils/handle_cancel";
 
 export default class MainCli {
-  private gitHooksScript = 'git config --local core.hooksPath .hooks/';
-  private packageJsonPath = path.join(path.dirname(''), 'package.json');
-  private hooksFolderPath = path.join(path.dirname(''), '.hooks');
+	private gitHooksScript = "git config --local core.hooksPath .hooks/";
+	private packageJsonPath = path.join(path.dirname(""), "package.json");
+	private hooksFolderPath = path.join(path.dirname(""), ".hooks");
 
-  public async prompt(): Promise<void> {
-    p.intro('Welcome to the git hook creator!');
+	public async prompt(): Promise<void> {
+		p.intro("Welcome to the git hook creator!");
 
-    const hook: string = (await p.select({
-      message: 'What hook do you want to create?',
-      options: GitHooks.map(hook => {
-        return {
-          value: hook.hook,
-          label: hook.hook,
-          hint: hook.link,
-        };
-      }),
-      maxItems: 8,
-    })) as string;
-    handleCancel(hook);
+		const hook: string = (await p.select({
+			message: "What hook do you want to create?",
+			options: GitHooks.map((hook) => {
+				return {
+					value: hook.hook,
+					label: hook.hook,
+					hint: hook.link,
+				};
+			}),
+			maxItems: 8,
+		})) as string;
+		handleCancel(hook);
 
-    const packageJson = await this.getPackageJson();
-    const { scripts } = await this.getPackageJsonContent(packageJson);
-    let scriptsChoosed: string[] = [];
+		const packageJson = await this.getPackageJson();
+		const { scripts } = await this.getPackageJsonContent(packageJson);
+		let scriptsChoosed: string[] = [];
 
-    if (!scripts) {
-      p.log.info('We couldn\'t find any script in your package.json.\nEnsure you have a script there so we can include it into your git hook.');
-    } else {
-      const availableScripts: { label: string; value: string }[] = [];
-      Object.keys(scripts).forEach(key => {
-        if (!key.startsWith('pre') && !key.startsWith('post')) availableScripts.push({
-          label: key,
-          value: key,
-        });
-      });
+		if (!scripts) {
+			p.log.info(
+				"We couldn't find any script in your package.json.\nEnsure you have a script there so we can include it into your git hook.",
+			);
+		} else {
+			const availableScripts: { label: string; value: string }[] = [];
 
-      scriptsChoosed = (await p.multiselect({
-        message: `Which scripts you want to execute on the ${hook} hook?`,
-        options: availableScripts,
-      })) as string[];
-      handleCancel(scriptsChoosed);
-    }
+			for (const key of Object.keys(scripts)) {
+				if (!key.startsWith("pre") && !key.startsWith("post"))
+					availableScripts.push({
+						label: key,
+						value: key,
+					});
+			}
 
-    // TODO: In the future we can choose which of these points need to be shown, based on the current user configuration.
-    p.log.info(
-      `For it to be done we will:\n- Create a .hooks folder if it doesn't already exist\n- Edit your prepare script in package.json\n- Create the .hooks/${hook} file`
-    );
+			scriptsChoosed = (await p.multiselect({
+				message: `Which scripts you want to execute on the ${hook} hook?`,
+				options: availableScripts,
+			})) as string[];
+			handleCancel(scriptsChoosed);
+		}
 
-    const okayToContinue = await p.confirm({
-      message: 'Do you want to apply changes now?',
-    });
-    handleCancel(okayToContinue);
+		// TODO: In the future we can choose which of these points need to be shown, based on the current user configuration.
+		p.log.info(
+			`For it to be done we will:\n- Create a .hooks folder if it doesn't already exist\n- Edit your prepare script in package.json\n- Create the .hooks/${hook} file`,
+		);
 
-    if (!okayToContinue) {
-      p.outro('Okay, we will not touch your file system ;)');
-      process.exit(0);
-    }
+		const okayToContinue = await p.confirm({
+			message: "Do you want to apply changes now?",
+		});
+		handleCancel(okayToContinue);
 
-    await this.createDotHooksFolder();
-    await this.createPrepareScript();
-    await this.createHookFile(hook, scriptsChoosed);
-    await this.makeHookFileExecutable(hook);
+		if (!okayToContinue) {
+			p.outro("Okay, we will not touch your file system ;)");
+			process.exit(0);
+		}
 
-    p.outro('Your hooks were added in .hooks folder.');
-  }
+		await this.createDotHooksFolder();
+		await this.createPrepareScript();
+		await this.createHookFile(hook, scriptsChoosed);
+		await this.makeHookFileExecutable(hook);
 
-  private async getPackageJsonContent(
-    packageJson: string
-  ): Promise<Record<string, any>> {
-    return JSON.parse(packageJson);
-  }
+		p.outro("Your hooks were added in .hooks folder.");
+	}
 
-  private async getPackageJson(): Promise<string> {
-    const file = await readFile(this.packageJsonPath, {
-      encoding: 'utf-8',
-    });
+	private async getPackageJsonContent(
+		packageJson: string,
+		// biome-ignore lint/suspicious/noExplicitAny: Since it's parsed from JSON it's difficult to say which type the value will be
+	): Promise<Record<string, any>> {
+		return JSON.parse(packageJson);
+	}
 
-    if (!file) {
-      p.log.error(
-        'Was not possible to read package.json.\nCheck if you are in a Node.js project folder.'
-      );
-      process.exit(1);
-    }
-    return file;
-  }
+	private async getPackageJson(): Promise<string> {
+		const file = await readFile(this.packageJsonPath, {
+			encoding: "utf-8",
+		});
 
-  private async createDotHooksFolder() {
-    try {
-      await access(this.hooksFolderPath);
-    } catch (_) {
-      await mkdir(this.hooksFolderPath);
-    }
-  }
+		if (!file) {
+			p.log.error(
+				"Was not possible to read package.json.\nCheck if you are in a Node.js project folder.",
+			);
+			process.exit(1);
+		}
+		return file;
+	}
 
-  private async createPrepareScript() {
-    let needToWriteFile = false;
-    const packageJson = await this.getPackageJson();
-    let packageJsonContent = await this.getPackageJsonContent(packageJson);
-    const scripts = packageJsonContent.scripts;
+	private async createDotHooksFolder() {
+		try {
+			await access(this.hooksFolderPath);
+		} catch (_) {
+			await mkdir(this.hooksFolderPath);
+		}
+	}
 
-    if (!scripts) {
-      packageJsonContent.scripts = {};
-    }
+	private async createPrepareScript() {
+		let needToWriteFile = false;
+		const packageJson = await this.getPackageJson();
+		const packageJsonContent = await this.getPackageJsonContent(packageJson);
+		const scripts = packageJsonContent.scripts;
 
-    if (!packageJsonContent.scripts.prepare) {
-      needToWriteFile = true;
-      packageJsonContent.scripts.prepare = this.gitHooksScript;
-    } else if (
-      !packageJsonContent.scripts.prepare.includes(this.gitHooksScript)
-    ) {
-      needToWriteFile = true;
-      packageJsonContent.scripts.prepare =
-        packageJsonContent.scripts.prepare + ' && ' + this.gitHooksScript;
-    }
+		if (!scripts) {
+			packageJsonContent.scripts = {};
+		}
 
-    if (needToWriteFile)
-      await writeFile(
-        this.packageJsonPath,
-        JSON.stringify(
-          packageJsonContent,
-          null,
-          this.getIndentation(packageJson)
-        )
-      );
-  }
+		if (!packageJsonContent.scripts.prepare) {
+			needToWriteFile = true;
+			packageJsonContent.scripts.prepare = this.gitHooksScript;
+		} else if (
+			!packageJsonContent.scripts.prepare.includes(this.gitHooksScript)
+		) {
+			needToWriteFile = true;
+			packageJsonContent.scripts.prepare = `${packageJsonContent.scripts.prepare} && ${this.gitHooksScript}`;
+		}
 
-  private getIndentation(packageJson: string) {
-    // capturing the first line break followed by empty spaces
-    const match = /\n(\s+)/.exec(packageJson);
-    // return either the captured emptyspaces or default
-    return match ? match[1] : '  ';
-  }
+		if (needToWriteFile)
+			await writeFile(
+				this.packageJsonPath,
+				JSON.stringify(
+					packageJsonContent,
+					null,
+					this.getIndentation(packageJson),
+				),
+			);
+	}
 
-  private async createHookFile(hook: string, scripts: string[]) {
-    const firstline = '#!/usr/bin/env sh';
-    const lastline = 'git add .';
+	private getIndentation(packageJson: string) {
+		// capturing the first line break followed by empty spaces
+		const match = /\n(\s+)/.exec(packageJson);
+		// return either the captured emptyspaces or default
+		return match ? match[1] : "  ";
+	}
 
-    const hookPath = path.join(path.dirname(''), '.hooks', hook);
-    let file: string;
+	private async createHookFile(hook: string, scripts: string[]) {
+		const firstline = "#!/usr/bin/env sh";
+		const lastline = "git add .";
 
-    try {
-      file = await readFile(hookPath, {
-        encoding: 'utf-8',
-      });
+		const hookPath = path.join(path.dirname(""), ".hooks", hook);
+		let file: string;
 
-      // TODO: add options to choose what to add/remove from the original hook file
-      const willContinue = await p.confirm({
-        message: `The file .hooks/${hook} already exists. Do you want to overwrite it?`,
-      });
-      handleCancel(willContinue);
+		try {
+			file = await readFile(hookPath, {
+				encoding: "utf-8",
+			});
 
-      if (!willContinue) {
-        p.outro('Okay, we will not touch your file system ;)');
-        process.exit(0);
-      }
-    } catch (error) {
-      file = '';
-    }
+			// TODO: add options to choose what to add/remove from the original hook file
+			const willContinue = await p.confirm({
+				message: `The file.hooks / ${hook} already exists.Do you want to overwrite it ? `,
+			});
+			handleCancel(willContinue);
 
-    const lines: string[] = [];
-    if (file)
-      lines.push(
-        ...file
-          .split('\n')
-          .filter(
-            line => line !== firstline && line !== lastline && line !== ''
-          )
-      );
+			if (!willContinue) {
+				p.outro("Okay, we will not touch your file system ;)");
+				process.exit(0);
+			}
+		} catch (error) {
+			file = "";
+		}
 
-    scripts.forEach(script => {
-      lines.push(`npm run ${script}`);
-    });
+		const lines: string[] = [];
+		if (file)
+			lines.push(
+				...file
+					.split("\n")
+					.filter(
+						(line) => line !== firstline && line !== lastline && line !== "",
+					),
+			);
 
-    const finalFile = [
-      firstline,
-      '', // space
-      ...lines,
-      '', // space
-      lastline,
-    ];
+		for (const script of scripts) {
+			lines.push(`npm run ${script}`);
+		}
 
-    await writeFile(hookPath, finalFile.join('\n'));
-  }
+		const finalFile = [
+			firstline,
+			"", // space
+			...lines,
+			"", // space
+			lastline,
+			"", // space
+		];
 
-  private async makeHookFileExecutable(hook: string) {
-    const hookPath = path.join(path.dirname(''), '.hooks', hook);
-    const executablePermissions = 0o755;
-    try {
-      await chmod(hookPath, executablePermissions);
-    } catch (error) {
-      p.log.error('We couldn\'t set the file as executable.\nYou can manually do it by running:\n\nchmod +x .hooks/*.sh');
-      process.exit(1);
-    }
-  }
+		await writeFile(hookPath, finalFile.join("\n"));
+	}
+
+	private async makeHookFileExecutable(hook: string) {
+		const hookPath = path.join(path.dirname(""), ".hooks", hook);
+		const executablePermissions = 0o755;
+		try {
+			await chmod(hookPath, executablePermissions);
+		} catch (error) {
+			p.log.error(
+				"We couldn't set the file as executable.\nYou can manually do it by running:\n\nchmod +x .hooks/*.sh",
+			);
+			process.exit(1);
+		}
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,37 @@
 #!/usr/bin/env node
-import { log } from '@clack/prompts';
-import { hideBin } from 'yargs/helpers';
-import yargs from 'yargs/yargs';
-import MainCli from './cli';
-import open from './utils/open_url';
+import { log } from "@clack/prompts";
+import { hideBin } from "yargs/helpers";
+import yargs from "yargs/yargs";
+import MainCli from "./cli";
+import open from "./utils/open_url";
 
 const yargsParameters = hideBin(process.argv);
 
 yargs(yargsParameters)
-  .scriptName('create-hook')
-  .usage('Usage: $0 <command> [options]')
-  .wrap(yargs(yargsParameters).terminalWidth())
-  .command('changelog', 'Open the tool changelog', () => {}, () => {
-    log.success('Changelog URL: https://github.com/hotaydev/git-hook-creator/releases');
-    open('https://github.com/hotaydev/git-hook-creator/releases');
-  })
-  .command('*', 'Create a new git hook', () => {}, () => {
-    new MainCli().prompt();
-  })
-  .help()
-  .version()
-  .alias('h', 'help')
-  .alias('v', 'version')
-  .parse();
+	.scriptName("create-hook")
+	.usage("Usage: $0 <command> [options]")
+	.wrap(yargs(yargsParameters).terminalWidth())
+	.command(
+		"changelog",
+		"Open the tool changelog",
+		() => {},
+		() => {
+			log.success(
+				"Changelog URL: https://github.com/hotaydev/git-hook-creator/releases",
+			);
+			open("https://github.com/hotaydev/git-hook-creator/releases");
+		},
+	)
+	.command(
+		"*",
+		"Create a new git hook",
+		() => {},
+		() => {
+			new MainCli().prompt();
+		},
+	)
+	.help()
+	.version()
+	.alias("h", "help")
+	.alias("v", "version")
+	.parse();
